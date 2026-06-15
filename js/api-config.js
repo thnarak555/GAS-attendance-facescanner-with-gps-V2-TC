113132bb5859d7df4e806777986d125efd433ef5
thnarak555 <thnarak555@gmail.com>
2026-06-15 08:49:16 +0700

    Add files via upload

---
+++ js/api-config.js
@@ -0,0 +1,5 @@
+// ============================================================
+//  ตั้งค่า Google Apps Script Web App URL
+//  แก้ไข URL ด้านล่างหากมีการ Deploy GAS ใหม่
+// ============================================================
+const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbzSB30mVkpg-J3eHWrje5hUwPj3K85uXF5Mn6a4tWB3_ckdGUqxYTBAaA8qrstir3oasQ/exec';
