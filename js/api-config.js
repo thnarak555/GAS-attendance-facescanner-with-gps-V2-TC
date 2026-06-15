7e59513d04c311f33b59200a08d6de8bdf6a1826
thnarak555 <thnarak555@gmail.com>
2026-06-15 09:28:24 +0700

    Update api-config.js

--- js/api-config.js
+++ js/api-config.js
@@ -2,4 +2,4 @@
 //  ตั้งค่า Google Apps Script Web App URL
 //  แก้ไข URL ด้านล่างหากมีการ Deploy GAS ใหม่
 // ============================================================
-const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbwnhij5ToY8L6woV9CsSzf2_VyNTBNTb1Fp5DNe-PgOVn9P1dSoadQjETRgkla1b8blLA/exec';
+const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbzeq7_glAo0163J4ZDY_Mvgx5H9uuetbSfaBFsfeXkvFgVKtZXxn6Z55Gz_-E5sGCMWxQ/exec';
